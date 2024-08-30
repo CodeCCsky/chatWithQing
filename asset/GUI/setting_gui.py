3470186555c1e2a8d46232b7edf4ca0e20816b83
CodeCCsky <164332311+CodeCCsky@users.noreply.github.com>
2024-08-31 07:50:11 +0800

    upd

--- asset/GUI/setting_gui.py
+++ asset/GUI/setting_gui.py
@@ -12,9 +12,15 @@ class SettingWidget(QWidget):
 
     def initUI(self):
         self.VLayout = QVBoxLayout(self)
-        self.sideListWidget = QListWidget(self)
-        self.VLayout.addWidget(self.sideListWidget)
-        self.
+        self.listsWidget = QWidget(self)
+        self.VLayout.addWidget(self.listsWidget)
+        self.listsHLayout = QHBoxLayout(self.listsWidget)
+        self.sideListWidget = QListWidget(self.listsWidget)
+        
+        self.listsHLayout.addWidget(self.sideListWidget)
+        self.mainOptionWidget = QWidget(self.listsWidget)
+        self.listsHLayout.addWidget(self.mainOptionWidget)
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
