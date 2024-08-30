916a679cbc7d61566049fa08dd12ed8c2619b9a2
CodeCCsky <164332311+CodeCCsky@users.noreply.github.com>
2024-08-30 23:42:03 +0800

    开始写设置界面

--- asset/GUI/setting_gui.py
+++ asset/GUI/setting_gui.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
+import sys
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
+# TODO
 class SettingWidget(QWidget):
     def __init__(self, parent: QWidget = None) -> None:
         super().__init__(parent)
+        self.initUI()
 
     def initUI(self):
-        pass#TODO
+        self.VLayout = QVBoxLayout(self)
+        self.sideListWidget = QListWidget(self)
+        self.VLayout.addWidget(self.sideListWidget)
+        self.
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    v0 = SettingWidget()
+    v0.show()
+    sys.exit(app.exec_())
