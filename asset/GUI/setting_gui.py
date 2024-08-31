90f2508ca2a104881c7a2199cbf397da64019cb8
CodeCCsky <164332311+CodeCCsky@users.noreply.github.com>
2024-08-31 17:30:20 +0800

    设置界面UI部分完成，TODO: 业务逻辑部分

--- asset/GUI/setting_gui.py
+++ asset/GUI/setting_gui.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
 import sys
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QApplication
+#from asset.GUI.Ui_setting import Ui_MainWindow
+from Ui_setting import Ui_MainWindow
+
 
 # TODO
-class SettingWidget(QWidget):
-    def __init__(self, parent: QWidget = None) -> None:
-        super().__init__(parent)
-        self.initUI()
 
-    def initUI(self):
-        self.VLayout = QVBoxLayout(self)
-        self.listsWidget = QWidget(self)
-        self.VLayout.addWidget(self.listsWidget)
-        self.listsHLayout = QHBoxLayout(self.listsWidget)
-        self.sideListWidget = QListWidget(self.listsWidget)
-        
-        self.listsHLayout.addWidget(self.sideListWidget)
-        self.mainOptionWidget = QWidget(self.listsWidget)
-        self.listsHLayout.addWidget(self.mainOptionWidget)
-        
+class SettingWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self) -> None:
+        super(SettingWidget, self).__init__()
+        self.setupUi(self)
+        self.initConnect()
+
+    def initConnect(self):
+        self.listWidget.itemClicked.connect(self.sideListWidgetClicked)
+
+    def sideListWidgetClicked(self, item):
+        index = self.listWidget.indexFromItem(item)
+        self.stackedWidget.setCurrentIndex(index.row())
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
