2dd3870a20bbbf1327ab9e85b3f924a353e23c38
CodeCCsky <164332311+CodeCCsky@users.noreply.github.com>
2024-09-03 01:47:03 +0800

    初始化界面和逻辑

--- asset/GUI/__init__.py
+++ asset/GUI/__init__.py
@@ -2,3 +2,4 @@ from asset.GUI.desktop_pet import DesktopPet
 from asset.GUI.input_label import inputLabel
 from asset.GUI.talk_bubble import talkBubble
 from asset.GUI.setting_gui import SettingWidget
+from asset.GUI.initialzation import initialzationWidget
