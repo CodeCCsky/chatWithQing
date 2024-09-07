a9745967fa531e837620516f8800b9ef9d27aade
CodeCCsky <164332311+CodeCCsky@users.noreply.github.com>
2024-09-07 13:09:10 +0800

    加载ui

--- asset/GUI/__init__.py
+++ asset/GUI/__init__.py
@@ -3,3 +3,4 @@ from asset.GUI.input_label import inputLabel
 from asset.GUI.talk_bubble import talkBubble
 from asset.GUI.setting_gui import SettingWidget
 from asset.GUI.initialzation import initialzationWidget
+from asset.GUI.load import loadWidget
