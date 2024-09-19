3add30d648e6069edfb2a01261c839ea14c88419
CodeCCsky <164332311+CodeCCsky@users.noreply.github.com>
2024-09-20 00:53:36 +0800

    upd

--- app/GUI/src/emotion_manager.py
+++ app/GUI/src/emotion_manager.py
@@ -1,20 +1,19 @@
-import sys
-from PyQt5.QtWidgets import QWidget, QApplication, QHBoxLayout, QListWidget
+from PyQt5.QtWidgets import QMainWindow
 
+from app.GUI.Ui.Ui_emotion_setting import Ui_MainWindow
+from third_party.emo_manager import emo_manager
 
-class emotionManager(QWidget):
-    def __init__(self, parent=None) -> None:
-        super().__init__(parent)
-        
-    def initUI(self):
-        self.vLayout = QHBoxLayout(self)
-        self.menuListWidget = QListWidget(self)
-        
-        #self.setLayout(self.vLayout)
+class emotionManagerWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self, emo_manager_: emo_manager, parent=None) -> None:
+        super(emotionManagerWidget, self).__init__(parent)
+        self.emotion_manager = emo_manager_
+        self.setupUi(self)
 
+    def init_connect(self):
+        self.categoryMenuListWidget.itemClicked.connect(self.sideListWidgetClicked)
+        pass
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    a = emotionManager()
-    a.show()
-    sys.exit(app.exec_())
+    def progress_menu_selected(self, item):
+        index = (self.categoryMenuListWidget.indexFromItem(item) - 1) / 2
+        print(index)
+
